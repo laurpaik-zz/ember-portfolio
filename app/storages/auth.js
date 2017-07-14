abce4bc019c8fcc84a4b593ebdc0f6e5f2c0a59c
laurpaik <laurpaik@gmail.com>
2017-07-14 14:50:46 -0400

    Clean up storage/auth

--- app/storages/auth.js
+++ app/storages/auth.js
@@ -1,12 +1,4 @@
 import StorageObject from 'ember-local-storage/local/object';
 
-const Storage = StorageObject.extend();
-
-// Uncomment if you would like to set initialState
-// Storage.reopenClass({
-//   initialState() {
-//     return {};
-//   }
-// });
-
-export default Storage;
+export default StorageObject.extend({
+});
